cd8fdf1545f2c5f3df35032dc839892e2844220b
Jesse Malone <jmalone@jsoft.ca>
2016-02-21 12:34:58 -0500

    fix blink.sh

--- blink.sh
+++ blink.sh
@@ -1,6 +1,6 @@
 while [ true ]; do
 	 sleep 6
-	 python rawimage.py imageinverted.pcx riboflav.in 26001
+	 python setimage.py imageinverted.pcx riboflav.in 26001
 	 sleep 6
-	 python rawimage.py image.pcx riboflav.in 26001
+	 python setimage.py image.pcx riboflav.in 26001
 done
